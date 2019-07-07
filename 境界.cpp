351218f19114e461a0565f0eef92458663bdb416
root <root@localhost>
2019-07-07 22:51:34 +0800

    test

---
+++ 境界.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    int jj;
+	int zj;
+	int jj1 = 1;
+	int jj10 = 10;
+	printf("%d,%d,%d\n",zj,jj1,jj10);
+	for (zj = 1;zj = zj*2;)
+	printf("%d\n",zj);
+	return 0;
+	}
