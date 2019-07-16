d835204ab2ff046b069ba9c5fb33ca8eb891392d
root <root@localhost>
2019-07-16 16:06:40 +0800

    求正弦

---
+++ Calculating sine.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+    double x,s;
+	printf("input number：\n");
+scanf("%lf",&x);	
+s= sin(x);
+	printf("sine of%lf is %lf\n",x,s);
+	return 0;
+	}
